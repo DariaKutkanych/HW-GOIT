519d2eb407342a71293c66d12382b40659e02213
Daria Kutkanych <daria_kutkanych@epam.com>
2022-10-11 11:14:46 +0300

    Create HW2 folder with data tasks

--- README.txt
+++
@@ -1,2 +0,0 @@
-To parse info from websites run scrapping.py
-To work with API run: uvicorn api:app
--- test_query.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect("news.db")
-
-cursor = connection.cursor()
-result = cursor.execute('''SELECT * FROM articles''')
-
-for res in result:
-    print(res)
